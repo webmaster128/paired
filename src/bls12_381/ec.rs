41156497df2727a73783ae17c695bcef78ef7eda
Jeff Burdges <burdges@gnunet.org>
2018-07-17 23:59:59 +0200

    Make batch_normalization more flexible with BorrowMut

--- src/bls12_381/ec.rs
+++ src/bls12_381/ec.rs
@@ -336,7 +336,8 @@ macro_rules! curve_impl {
                 self.is_zero() || self.z == $basefield::one()
             }
 
-            fn batch_normalization(v: &mut [Self]) {
+            fn batch_normalization<S: ::std::borrow::BorrowMut<Self>>(v: &mut [S])
+            {
                 // Montgomery’s Trick and Fast Implementation of Masked AES
                 // Genelle, Prouff and Quisquater
                 // Section 3.2
@@ -344,8 +345,8 @@ macro_rules! curve_impl {
                 // First pass: compute [a, ab, abc, ...]
                 let mut prod = Vec::with_capacity(v.len());
                 let mut tmp = $basefield::one();
-                for g in v
-                    .iter_mut()
+                for g in v.iter_mut()
+	            .map(|g| g.borrow_mut())
                     // Ignore normalized elements
                     .filter(|g| !g.is_normalized())
                 {
@@ -357,17 +358,15 @@ macro_rules! curve_impl {
                 tmp = tmp.inverse().unwrap(); // Guaranteed to be nonzero.
 
                 // Second pass: iterate backwards to compute inverses
-                for (g, s) in v
-                    .iter_mut()
+                for (g, s) in v.iter_mut()
+                    .map(|g| g.borrow_mut())
                     // Backwards
                     .rev()
                     // Ignore normalized elements
                     .filter(|g| !g.is_normalized())
                     // Backwards, skip last element, fill in one for last term.
                     .zip(
-                        prod.into_iter()
-                            .rev()
-                            .skip(1)
+                        prod.into_iter().rev().skip(1)
                             .chain(Some($basefield::one())),
                     )
                 {
@@ -380,7 +379,10 @@ macro_rules! curve_impl {
                 }
 
                 // Perform affine transformations
-                for g in v.iter_mut().filter(|g| !g.is_normalized()) {
+                for g in v.iter_mut()
+                    .map(|g| g.borrow_mut())
+                    .filter(|g| !g.is_normalized())
+                {
                     let mut z = g.z; // 1/z
                     z.square(); // 1/z^2
                     g.x.mul_assign(&z); // x/z^2
