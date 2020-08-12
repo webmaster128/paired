6e584a944af1c2f5e9dee7308e6190d19b1aaaaa
DrPeterVanNostrand <jnz@riseup.net>
2020-08-12 12:03:57 -0400

    feat: allow subgroup checks when deserializing raw G1

--- src/bls12_381/ec/g1.rs
+++ src/bls12_381/ec/g1.rs
@@ -1,5 +1,5 @@
 use std::fmt;
-use std::io::{Read, Write};
+use std::io::{self, Read, Write};
 
 use fff::{BitIterator, Field, PrimeField, PrimeFieldRepr, SqrtField};
 use groupy::{CurveAffine, CurveProjective, EncodedPoint, GroupDecodingError};
@@ -60,7 +60,7 @@ impl G1Affine {
         Ok(Self::raw_fmt_size())
     }
 
-    pub fn read_raw<R: Read>(mut reader: R) -> Result<Self, std::io::Error> {
+    pub fn read_raw<R: Read>(mut reader: R, check: bool) -> Result<Self, std::io::Error> {
         let mut buf = [0u8];
         reader.read_exact(&mut buf)?;
         let infinity = buf[0] == 1;
@@ -68,11 +68,29 @@ impl G1Affine {
         x.read_be(&mut reader)?;
         let mut y = FqRepr::default();
         y.read_be(&mut reader)?;
-        Ok(Self {
+
+        let affine = Self {
             x: Fq(x),
             y: Fq(y),
             infinity,
-        })
+        };
+
+        if check {
+            if !affine.is_on_curve() {
+                return Err(io::Error::new(
+                    io::ErrorKind::InvalidData,
+                    GroupDecodingError::NotOnCurve,
+                ));
+            }
+            if !affine.is_in_correct_subgroup_assuming_on_curve() {
+                return Err(io::Error::new(
+                    io::ErrorKind::InvalidData,
+                    GroupDecodingError::NotInSubgroup,
+                ));
+            }
+        }
+
+        Ok(affine)
     }
 }
 
@@ -1712,7 +1730,7 @@ mod tests {
             let bytes_written = affine.write_raw(&mut buf).unwrap();
             assert_eq!(G1Affine::raw_fmt_size(), bytes_written);
             assert_eq!(G1Affine::raw_fmt_size(), buf.len());
-            let affine_again = G1Affine::read_raw(buf.as_slice()).unwrap();
+            let affine_again = G1Affine::read_raw(buf.as_slice(), true).unwrap();
 
             assert_eq!(affine, affine_again);
         }
