1020814eedbd65b94e2f91275d84e774edccd655
Simon Warta <simon@warta.it>
2020-08-17 18:17:52 +0200

    Re-export traits CurveAffine, CurveProjective, EncodedPoint

--- src/lib.rs
+++ src/lib.rs
@@ -28,7 +28,7 @@ pub use self::hash_to_field::{hash_to_field, BaseFromRO, ExpandMsgXmd, ExpandMsg
 pub use self::signum::{Sgn0Result, Signum0};
 
 use fff::{Field, PrimeField, ScalarEngine, SqrtField};
-use groupy::{CurveAffine, CurveProjective};
+pub use groupy::{CurveAffine, CurveProjective, EncodedPoint};
 
 #[cfg(feature = "serde")]
 #[macro_use]
